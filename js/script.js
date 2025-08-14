c100406f6d10e355850de7a914b7f5a8f1bbc415
Diógenes Vieira <jdiogenesvieira@gmail.com>
2025-08-14 16:04:21 +0100

    melhoramentos

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Menu hambúrguer
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
